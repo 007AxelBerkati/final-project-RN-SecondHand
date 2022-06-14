80cc61c489c1bda742dd193fe14e044fe0bf0d36
anisafsp03 <anisafebryantisucipramesti@gmail.com>
2022-06-14 19:22:57 +0700

    add color

--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -6,13 +6,18 @@ const mainColors = {
   grey3: '#E9E9E9',
   grey4: '#EDEEF0',
   grey5: '#B1B7C2',
-  dark1: '#112340',
-  dark2: '#495A75',
-  red1: '#E06379',
-  blue1: '#2F80ED',
-  blue2: '#0066CB',
+  // dark1: '#112340',
+  // dark2: '#495A75',
+  // red1: '#E06379',
+  // blue1: '#2F80ED',
+  // blue2: '#0066CB',
   green1: '#0BCAD4',
   green2: '#EDFCFD',
+  orange1: '#FF8303',
+  orange2: '#FF9425',
+  orange3: '#FFA84D',
+  orange4: '#FFB669',
+  orange5: '#FFCB94',
 };
 
 export const colors = {
