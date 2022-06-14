15728cf5bac8ee1d5e93f4eab62bb4c5db194539
anisafsp03 <anisafebryantisucipramesti@gmail.com>
2022-06-14 18:01:54 +0700

    export font

---
+++ src/utils/fonts/index.js
@@ -0,0 +1,9 @@
+export const fonts = {
+  Poppins: {
+    Regular: 'Poppins-Regular',
+    Light: 'Poppins-Light',
+    Bold: 'Poppins-Bold',
+    Medium: 'Poppins-Medium',
+    SemiBold: 'Poppins-SemiBold',
+  },
+};
