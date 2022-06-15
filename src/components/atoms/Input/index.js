8d1d248b81c9094c9d6c42b1f4bbee70d0e0d7ea
anisafsp03 <anisafebryantisucipramesti@gmail.com>
2022-06-15 15:19:44 +0700

    update

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,10 +1,16 @@
-import React, { memo, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { TextInput } from 'react-native-paper';
-import { colors, fonts } from '../../../utils';
+import React, {memo, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {TextInput} from 'react-native-paper';
+import {colors, fonts} from '../../../utils';
 
-function Input2({
-  onChangeText, value, label, onBlur, cannotEdited, secureTextEntry, leftIcon,
+function Input({
+  onChangeText,
+  value,
+  label,
+  onBlur,
+  cannotEdited,
+  secureTextEntry,
+  leftIcon,
 }) {
   const [passwordVisible, setPasswordVisible] = useState(false);
   return (
@@ -16,26 +22,27 @@ function Input2({
         onBlur={onBlur}
         mode="outlined"
         activeOutlineColor={colors.lineTextInput}
-        outlineColor={cannotEdited ? colors.disable.background : colors.outlineInput}
+        outlineColor={
+          cannotEdited ? colors.disable.background : colors.outlineInput
+        }
         style={styles.input}
         secureTextEntry={secureTextEntry ? passwordVisible : false}
-        left={(
-          <TextInput.Icon name={leftIcon} />
-            )}
-        right={secureTextEntry ? (
-          <TextInput.Icon
-            name={passwordVisible ? 'eye' : 'eye-off'}
-            onPress={() => setPasswordVisible(!passwordVisible)}
-            color={passwordVisible ? colors.background.black : colors.warning}
-          />
-        ) : null}
-
+        left={<TextInput.Icon name={leftIcon} />}
+        right={
+          secureTextEntry ? (
+            <TextInput.Icon
+              name={passwordVisible ? 'eye' : 'eye-off'}
+              onPress={() => setPasswordVisible(!passwordVisible)}
+              color={passwordVisible ? colors.background.black : colors.warning}
+            />
+          ) : null
+        }
       />
     </View>
   );
 }
 
-export default memo(Input2);
+export default memo(Input);
 
 const styles = StyleSheet.create({
   input: {
@@ -44,4 +51,3 @@ const styles = StyleSheet.create({
     color: colors.text.secondary,
   },
 });
-
