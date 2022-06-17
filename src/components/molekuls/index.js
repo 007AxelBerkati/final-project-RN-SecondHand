14761c4689be6e95e96251ecab0358cf24ed35ca
007AxelBerkati <bucel1916@gmail.com>
2022-06-17 21:12:24 +0700

    update slicing

--- src/components/molekuls/index.js
+++ src/components/molekuls/index.js
@@ -3,7 +3,8 @@ import CardProduct from './CardProduct';
 import Headers from './Headers';
 import CardList from './CardList';
 import Loading from './Loading';
+import Profile from './Profile';
 
 export {
-  CardCategory, CardProduct, Headers, CardList, Loading,
+  CardCategory, CardProduct, Headers, CardList, Loading, Profile,
 };
--- src/redux/reducer/akunReducer.js
+++ src/redux/reducer/akunReducer.js
@@ -1,31 +1,23 @@
-import { GET_AKUN_FAIL, GET_AKUN_LOADING, GET_AKUN_SUCCESS } from '../types';
+import { GET_AKUN_FAIL, GET_AKUN_SUCCESS } from '../types';
 
 const initialStateAkun = {
   dataAkun: {},
-  isLoading: false,
   isSuccess: false,
   error: '',
 };
 
 export const akunReducer = (state = initialStateAkun, action = {}) => {
   switch (action.type) {
-    case GET_AKUN_LOADING:
-      return {
-        ...state,
-        isLoading: action.payload,
-      };
     case GET_AKUN_SUCCESS:
       return {
         ...state,
         isSuccess: true,
-        isLoading: false,
         dataAkun: action.payload,
       };
     case GET_AKUN_FAIL:
       return {
         ...state,
         isSuccess: false,
-        isLoading: false,
         error: action.payload,
       };
     default:
