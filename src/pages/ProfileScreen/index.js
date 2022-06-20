77f1b29af1b6db9e304083721b617714f10f1324
Fernando Brian <fernando.brian0501@gmail.com>
2022-06-20 13:50:25 +0700

    lanjutan profile screen

--- src/pages/ProfileScreen/index.js
+++ src/pages/ProfileScreen/index.js
@@ -2,19 +2,23 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
   TextInput,
   Button,
-  Image,
+  TouchableOpacity,
 } from 'react-native';
-import React from 'react';
-import {Icon} from 'react-native-vector-icons/Ionicons';
-
-import {Avatar} from '@rneui/themed';
-
+import React, {useState} from 'react';
+import {fonts} from '../../utils';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {windowHeight, windowWidth} from '../../utils/dimensions';
+import Input from '../../components/atoms/Input';
+import {Formik} from 'formik';
+import SelectDropdown from 'react-native-select-dropdown';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Profile from '../../components/molekuls/Profile';
+import {ILNullPhoto} from '../../assets';
+import Headers from '../../components/molekuls/Headers';
 
-function ProfileScreen() {
+function ProfileScreen({navigation}) {
   const getImage = () => {
     launchImageLibrary(
       {
@@ -39,46 +43,130 @@ function ProfileScreen() {
     );
   };
 
+  const kota = [
+    'Ambon',
+    'Balikpapan',
+    'Banda Aceh',
+    'Bandar Lampung',
+    'Bandung',
+    'Banjar',
+    'Banjarbaru',
+    'Banjarmasin',
+    'Batam',
+    'Batu',
+    'Baubau',
+    'Bekasi',
+    'Bengkulu',
+    'Bima',
+    'Binjai',
+    'Bitung',
+    'Blitar',
+    'Bogor',
+    'Bontang',
+    'Bukittinggi',
+    'Cilegon',
+    'Cimahi',
+    'Cirebon',
+    'Denpasar',
+    'Depok',
+    'Dumai',
+    'Gorontalo',
+    'Gunungsitoli',
+    'Jakarta Barat',
+    'Jakarta Pusat',
+    'Jakarta Selatan',
+    'Jakarta Timur',
+    'Jakarta Utara',
+    'Jambi',
+    'Jayapura',
+    'Kediri',
+  ];
+
   return (
     <View style={styles.pages}>
-      <View style={styles.header}>
-        <View style={styles.icon1}>
-          <Ionicons name="arrow-back-outline" color="black" size={20} />
-        </View>
-        <View>
-          <Text style={styles.text1}>Lengkapi Info Akun</Text>
-        </View>
-      </View>
-      <View style={styles.name}>
-        <TextInput
-          style={styles.input1}
-          placeholder="Nama"
-          placeholderTextColor={'black'}
-          selectionColor={'black'}
-        />
-        <TextInput
-          style={styles.input2}
-          placeholder="Kota"
-          placeholderTextColor={'black'}
-          selectionColor={'black'}
-        />
-        <TextInput
-          style={styles.input3}
-          placeholder="Alamat"
-          placeholderTextColor={'black'}
-          selectionColor={'black'}
-        />
-        <TextInput
-          style={styles.input4}
-          placeholder="No Handphone"
-          placeholderTextColor={'black'}
-          selectionColor={'black'}
+      <View>
+        <Headers
+          title="Lengkapi Info Akun"
+          type="back-title"
+          onPress={() => navigation.goBack()}
         />
       </View>
+      <View style={styles.photo}>
+        <Profile source={ILNullPhoto} />
+      </View>
+      <Formik
+        initialValues={{nama: '', alamat: '', handphone: ''}}
+        onSubmit={values => console.log(values)}>
+        {({handleChange, handleBlur, handleSubmit, values}) => (
+          <View style={{paddingHorizontal: 5, margin: 15}}>
+            <Input
+              leftIcon="account"
+              label="Nama"
+              onChangeText={handleChange('nama')}
+              value={values.nama}
+              onBlur={handleBlur('nama')}
+            />
 
-      <View style={styles.Button}>
-        <Button title="Simpan" color="red" />
-      </View>
+            <SelectDropdown
+              data={kota}
+              // defaultValueByIndex={1}
+              // defaultValue={'Egypt'}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index);
+              }}
+              defaultButtonText={'Pilih Kota'}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem;
+              }}
+              rowTextForSelection={(item, index) => {
+                return item;
+              }}
+              buttonStyle={styles.dropdown1BtnStyle}
+              buttonTextStyle={styles.dropdown1BtnTxtStyle}
+              renderDropdownIcon={isOpened => {
+                return (
+                  <FontAwesome
+                    name={isOpened ? 'chevron-up' : 'chevron-down'}
+                    color={'#444'}
+                    size={18}
+                  />
+                );
+              }}
+              dropdownIconPosition={'right'}
+              dropdownStyle={styles.dropdown1DropdownStyle}
+              rowStyle={styles.dropdown1RowStyle}
+              rowTextStyle={styles.dropdown1RowTxtStyle}
+              selectedRowStyle={styles.dropdown1SelectedRowStyle}
+              search
+              searchInputStyle={styles.dropdown1searchInputStyleStyle}
+              searchPlaceHolder={'Search here'}
+              searchPlaceHolderColor={'darkgrey'}
+              renderSearchInputLeftIcon={() => {
+                return <FontAwesome name={'search'} color={'#444'} size={18} />;
+              }}
+            />
+
+            <Input
+              leftIcon="home"
+              label="Alamat"
+              onChangeText={handleChange('alamat')}
+              value={values.alamat}
+              onBlur={handleBlur('alamat')}
+            />
+            <Input
+              leftIcon="cellphone"
+              label="No. Handphone"
+              onChangeText={handleChange('handphone')}
+              value={values.handphone}
+              onBlur={handleBlur('handphone')}
+            />
+
+            <View style={styles.Button}>
+              <Button title="Simpan" color="red" />
+            </View>
+          </View>
+        )}
+      </Formik>
     </View>
   );
 }
@@ -87,52 +175,79 @@ export default ProfileScreen;
 
 const styles = StyleSheet.create({
   pages: {
-    justifyContent: 'center',
-    alignItems: 'center',
     flex: 1,
     backgroundColor: 'white',
   },
-  header: {
-    flexDirection: 'row',
-    marginTop: -50,
+  borderText: {
+    marginLeft: windowWidth * 0.3,
+    marginTop: windowHeight * -0.06,
   },
   text1: {
     color: '#112340',
-    marginRight: 120,
+    fontSize: 16,
+    marginTop: 10,
+    fontFamily: fonts.Poppins.SemiBold,
   },
   icon1: {
-    marginRight: 105,
+    marginRight: 64,
+  },
+  name: {
+    marginTop: windowHeight * 0.3,
   },
   input1: {
     borderWidth: 1,
-    height: 40,
-    width: 350,
-    marginTop: 170,
+    height: 50,
+    width: windowWidth * 0.93,
     borderRadius: 12,
-  },
-  input2: {
-    borderWidth: 1,
-    height: 40,
-    width: 350,
-    borderRadius: 12,
-    marginTop: 35,
-  },
-  input3: {
-    borderWidth: 1,
-    height: 90,
-    width: 350,
-    borderRadius: 12,
-    marginTop: 35,
-  },
-  input4: {
-    borderWidth: 1,
-    height: 40,
-    width: 350,
-    borderRadius: 12,
-    marginTop: 35,
   },
   Button: {
-    width: 200,
-    margin: 50,
+    width: 250,
+    marginLeft: windowWidth * 0.13,
+    marginTop: windowHeight * 0.13,
+  },
+
+  text: {
+    fontSize: 24,
+  },
+  kota: {
+    borderWidth: 1,
+    borderRadius: 12,
+    marginTop: 15,
+  },
+  dropdown1BtnStyle: {
+    width: windowWidth * 0.9,
+    height: 50,
+    backgroundColor: '#F0F0F0',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#B0B0B0',
+    marginTop: 6,
+  },
+  dropdown1BtnTxtStyle: {
+    color: '#444',
+    textAlign: 'left',
+  },
+  dropdown1DropdownStyle: {
+    backgroundColor: '#EFEFEF',
+  },
+  dropdown1RowStyle: {
+    backgroundColor: '#EFEFEF',
+    borderBottomColor: '#C5C5C5',
+  },
+  dropdown1RowTxtStyle: {
+    color: '#444',
+    textAlign: 'left',
+  },
+  dropdown1SelectedRowStyle: {
+    backgroundColor: 'rgba(0,0,0,0.1)',
+  },
+  dropdown1searchInputStyleStyle: {
+    backgroundColor: '#EFEFEF',
+    borderRadius: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#444',
+  },
+  photo: {
+    marginTop: windowHeight * 0.04,
   },
 });
