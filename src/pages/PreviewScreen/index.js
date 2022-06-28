1a5e5ffc10dc455aa21d7f42ae316298ae0935fe
007AxelBerkati <bucel1916@gmail.com>
2022-06-28 20:03:06 +0700

    update slicing, change using banner

--- src/pages/PreviewScreen/index.js
+++ src/pages/PreviewScreen/index.js
@@ -1,27 +1,35 @@
 import {
   SafeAreaView, ScrollView, StyleSheet, View,
   StatusBar,
-  Image,
 } from 'react-native';
 import React from 'react';
-import { moderateScale } from 'react-native-size-matters';
+import { ImageSlider } from 'react-native-image-slider-banner';
 import {
   ButtonComponent, CardList, CardProduct, Desc, Gap,
 } from '../../components';
 import { keranjang } from '../../assets';
 import {
-  colors, fonts, fontSize, windowHeight, windowWidth,
+  colors, windowHeight, windowWidth,
 } from '../../utils';
 
 function PreviewScreen() {
   return (
     <SafeAreaView style={styles.pages}>
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator>
         <StatusBar backgroundColor="transparent" translucent />
         <View style={styles.imageContainer}>
-          <Image
-            style={styles.image}
-            source={keranjang}
+          <ImageSlider
+            data={[
+              { img: 'https://img.freepik.com/free-vector/online-shopping-concept-illustration_114360-1084.jpg?w=1060&t=st=1655909482~exp=1655910082~hmac=17398ae027564d6d34625f5b83f8ab39616da2ea74d55677a11eb56ce8eb70bd' },
+              { img: 'https://media.istockphoto.com/photos/male-outfit-set-on-white-background-closeup-top-view-picture-id1176614800?k=20&m=1176614800&s=612x612&w=0&h=uM0CkYhsM12_OYWhnO641tXqD3ZCOx61dyGZClkrv6k=' },
+              { img: 'https://images.unsplash.com/photo-1479064555552-3ef4979f8908?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' },
+            ]}
+            autoPlay
+            timer={5000}
+            closeIconColor={colors.background.primary}
+            caroselImageStyle={{ height: windowHeight * 0.4 }}
+            indicatorContainerStyle={{ bottom: windowHeight * 0.05 }}
+
           />
           <View style={styles.btnBackContainer}>
             <ButtonComponent
@@ -49,70 +57,6 @@ function PreviewScreen() {
       <View style={styles.btnNego}>
         <ButtonComponent title="Terbitkan" />
       </View>
-
-      {/* <BottomSheet
-        enablePanDownToClose
-        enableContentPanningGesture
-        enableHandlePanningGesture
-        animateOnMount
-        enableOverDrag
-        ref={bottomSheetRef}
-        index={-1}
-        snapPoints={snapPoints}
-        onChange={handleSheetChanges}
-      >
-        <View style={styles.contentContainer}>
-          <Text style={styles.titleBtmSheet}>Masukkan Harga Tawarmu</Text>
-          <Gap height={moderateScale(16)} />
-          <Text style={styles.descBtmSheet}>
-          </Text>
-          <Gap height={moderateScale(16)} />
-
-          <View style={styles.mainCard}>
-            <View style={styles.icon}>
-              <FastImage
-                source={stateBuyer?.productDetail?.image_url}
-                style={styles.imageBtmSheet}
-                resizeMode="cover"
-              />
-            </View>
-            <View style={styles.contentText}>
-              <Text style={styles.name}>{stateBuyer?.productDetail?.name}</Text>
-              <Text>
-                Rp
-                {' '}
-                {stateBuyer?.productDetail?.base_price}
-              </Text>
-            </View>
-          </View>
-
-          <Gap height={moderateScale(16)} />
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <BaseInput
-                label="Harga Tawar"
-                type="text"
-                placeholder="Rp. 0,00"
-                onChangeText={onChange}
-                onBlur={onBlur}
-                value={value}
-              />
-            )}
-            name="bid_price"
-            rules={{ required: { value: true, message: 'Silahkan Isi Penawaran' } }}
-          />
-          {errors?.bid_price && <Text style={styles.errors}>{errors.bid_price.message}</Text>}
-
-          <Gap height={moderateScale(24)} />
-          <BaseButton
-            disable={stateBuyer?.isLoadingBid}
-            isLoading={stateBuyer?.isLoadingBid}
-            title="Kirim"
-            onPress={handleSubmit(async (data) => await onSubmit(data))}
-          />
-        </View>
-      </BottomSheet> */}
     </SafeAreaView>
   );
 }
@@ -131,36 +75,21 @@ const styles = StyleSheet.create({
     left: 16,
   },
 
-  image: {
-    height: '80%',
-    width: '100%',
-  },
   title: {
     marginTop: -12,
   },
+
   card: {
     marginHorizontal: 16,
     marginBottom: 30,
     marginTop: -24,
-  },
-  txtTitle: {
-    fontFamily: fonts.Poppins.Regular,
-    fontSize: moderateScale(14),
-    color: 'black',
-  },
-  txtCat: {
-    fontFamily: fonts.Poppins.Regular,
-    fontSize: moderateScale(10),
-  },
-  des: {
-    fontFamily: fonts.Poppins.Regular,
-    fontSize: moderateScale(14),
   },
 
   pages: {
     flex: 1,
     backgroundColor: colors.background.primary,
   },
+
   btnNego: {
     padding: 16,
     position: 'absolute',
@@ -168,70 +97,9 @@ const styles = StyleSheet.create({
     bottom: 1,
   },
 
-  container: {
-    flex: 1,
-    padding: moderateScale(24),
-    backgroundColor: colors.background.secondary,
-  },
-  contentContainer: {
-    flex: 1,
-    padding: moderateScale(32),
-  },
-  titleBtmSheet: {
-    fontFamily: fonts.Poppins.Medium,
-    fontSize: moderateScale(14),
-    color: colors.text.primary,
-  },
-  descBtmSheet: {
-    fontFamily: fonts.Poppins.Regular,
-    fontSize: moderateScale(14),
-    color: colors.background.secondary,
-  },
-
-  //   Styles for Card Product
-  mainCard: {
-    height: moderateScale(80),
-    width: '100%',
-    alignItems: 'center',
-    borderRadius: moderateScale(16),
-    backgroundColor: 'transparent',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.15,
-    shadowRadius: 4,
-    elevation: 1,
-    flexDirection: 'row',
-  },
-  icon: {
-    marginLeft: moderateScale(18),
-  },
-  name: {
-    fontFamily: fonts.Poppins.Bold,
-    fontSize: moderateScale(14),
-    color: colors.text.primary,
-  },
-  contentText: {
-    marginLeft: moderateScale(16),
-  },
-  imageBtmSheet: {
-    width: moderateScale(48),
-    height: moderateScale(48),
-    borderRadius: moderateScale(12),
-    backgroundColor: colors.background.tertiary,
-  },
-  loading: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  errorText: {
-    fontFamily: fonts.Poppins.Medium,
-    color: colors.warning,
-    fontSize: fontSize.small,
-  },
   productWrapper: {
     marginHorizontal: 16,
-    marginTop: moderateScale(-40),
+    marginTop: -40,
   },
 
 });
